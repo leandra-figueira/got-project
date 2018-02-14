e8551a065aec2c4025fa612d9078ebdfb2897c3e
Leandra <leandra.figueira@gmail.com>
2018-02-14 10:24:27 -0600

    Updated the seed.rb file to populate the tables Season and Episode with data from TVMaze API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# TVMAZE
+url_season = 'http://api.tvmaze.com/shows/82/seasons'
+uri_season = URI(url_season)
+response = Net::HTTP.get(uri_season)
+parsed_season = JSON.parse(response)
+
+Season.destroy_all
+Episode.destroy_all
+
+parsed_season.each do |d|
+  # inserting data on season table
+  Season.create(
+    id: d['id'],
+    season_number: d['number'],
+    premiere_date: d['premiereDate'],
+    end_date: d['endDate'],
+    summary: d['summary']
+  )
+  i = d['id']
+  # searching episodes from this season
+  url = 'http://api.tvmaze.com/seasons/' + i.to_s + '/episodes'
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  parsed_episode = JSON.parse(response)
+
+  parsed_episode.each do |e|
+    Episode.create(
+     id: e['id'],
+     season_id: d['id'],
+     episode_number: e['number'],
+     name: e['name'],
+     date: e['date'],
+     summary: e['summary']
+    )
+  end
+end
+
+puts "Seed generated #{Season.count} seasons and #{Episode.count} episodes"
